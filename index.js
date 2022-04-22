b4d419f76fcaf00426f66cbc9439ba75ee5f26f6
Batfos <batfos@gmail.com>
2022-04-22 18:49:12 +0700

    App available on http://localhost:3000

--- index.js
+++ index.js
@@ -55,6 +55,23 @@
 
 // 7. MODULES & NPM
 
-const myModule = require('./my-module')
+// const myModule = require('./my-module')
 
-console.log(myModule)
+// console.log(myModule)
+
+const express = require('express')
+const {readFile} = require('fs')
+
+const app = express()
+
+app.get('/', (request, response) => {
+  readFile('./beranda.html', 'utf8', (err, html) => {
+    if (err) {
+      response.status(500).send('Sorry, out of order')
+    }
+
+    response.send(html)
+  })
+})
+
+app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
