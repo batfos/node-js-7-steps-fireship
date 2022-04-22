e8e8f9de98ad5377a2e22f828388a8f7b2ffaf23
Batfos <batfos@gmail.com>
2022-04-22 15:59:01 +0700

    Astaghfirullāh 🧎

--- index.js
+++ index.js
@@ -2,12 +2,26 @@
 
 // 4. KNOW YOUR RUNTIME
 
-console.log(global.tanggalRamadan)
+// console.log(global.tanggalRamadan)
 
-global.tanggalRamadan = '20'
+// global.tanggalRamadan = '20'
 
-console.log(global.tanggalRamadan)
+// console.log(global.tanggalRamadan)
 
-console.log(process.platform)
+// console.log(process.platform)
 
-console.log(process.env.USER)
+// console.log(process.env.USER)
+
+// 5. EVENTS
+
+const {EventEmitter} = require('events')
+
+const eventEmitter = new EventEmitter()
+
+eventEmitter.on('istigfar', () => {
+  console.log('Astaghfirullāh 🧎')
+})
+
+eventEmitter.emit('istigfar') // Astaghfirullāh 🧎
+eventEmitter.emit('istigfar') // Astaghfirullāh 🧎
+eventEmitter.emit('istigfar') // Astaghfirullāh 🧎
