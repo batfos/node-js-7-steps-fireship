9bdc8460d649591be96eed6034689e31fbe0ba3f
Batfos <batfos@gmail.com>
2022-04-22 22:30:16 +0700

    Aplikasi tersedia di http://localhost:3000
    - `promises`

--- index.js
+++ index.js
@@ -60,18 +60,12 @@
 // console.log(myModule)
 
 const express = require('express')
-const {readFile} = require('fs')
+const {readFile} = require('fs').promises
 
 const app = express()
 
-app.get('/', (request, response) => {
-  readFile('./beranda.html', 'utf8', (err, html) => {
-    if (err) {
-      response.status(500).send('Sorry, out of order')
-    }
-
-    response.send(html)
-  })
+app.get('/', async (request, response) => {
+  response.send(await readFile('./beranda.html', 'utf8'))
 })
 
-app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
+app.listen(process.env.PORT || 3000, () => console.log(`Aplikasi tersedia di http://localhost:3000`))
