9feb82e8605cf2896ea46f27a8a9e00dc08720ce
Batfos <batfos@gmail.com>
2022-04-22 15:26:58 +0700

    4. KNOW YOUR RUNTIME

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Salam, Dunia! 👋')
+// console.log('Salam, Dunia! 👋')
+
+// 4. KNOW YOUR RUNTIME
+
+console.log(global.tanggalRamadan)
+
+global.tanggalRamadan = '20'
+
+console.log(global.tanggalRamadan)
+
+console.log(process.platform)
+
+console.log(process.env.USER)
